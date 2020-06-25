ad10053776fd16fad5b1c7f617a0ad7af59b010b
juanjpl <58012316+juanjpl@users.noreply.github.com>
2020-06-25 15:08:50 -0300

    Add files via upload

--- app/src/main/java/com/example/convertidoralpha/MainActivity.java
+++ app/src/main/java/com/example/convertidoralpha/MainActivity.java
@@ -3,13 +3,81 @@ package com.example.convertidoralpha;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //propiedades tipo nombre = valor
+    //Creo los Views que voy a usar
+
+    EditText temperaturaET;
+    Button fahrBT;
+    Button celBT;
+    TextView temperaturaTV;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Asocio los Views a sus contrapartes XML
+
+        temperaturaET   = findViewById(R.id.temperaturaET);
+        fahrBT          = findViewById(R.id.fahrBT);
+        celBT           = findViewById(R.id.celBT);
+        temperaturaTV   = findViewById(R.id.temperaturaTV);
+
+        //temperaturaTV.setText("Hola Mundo!");
+
+
+        View.OnClickListener seClickeoFahr = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                // traer el valor temperaturaET
+
+                //parsear String a Float
+                String temperaturaSTR = temperaturaET.getText().toString();
+                Float temperaturaFLT = Float.parseFloat(temperaturaSTR);
+
+
+
+                //convertir
+                Float resultado = ( temperaturaFLT + 9/5 ) + 32;
+
+
+
+                //mostrar el valor
+                temperaturaTV.setText(resultado.toString());
+
+
+
+            }
+        };
+
+        fahrBT.setOnClickListener(seClickeoFahr);
+
+    }
+
+    //Funcion auxiliar
+    public void seClickeoCel(View view){
+
+        // traer el valor temperaturaET
+
+        //parsear String a Float
+        String temperaturaSTR = temperaturaET.getText().toString();
+        Float temperaturaFLT = Float.parseFloat(temperaturaSTR);
+
+        //convertir
+        Float resultado = ( temperaturaFLT - 32 ) * 5/9;
+
+        //mostrar el valor
+        temperaturaTV.setText(resultado.toString());
     }
 }
